a1f9e92c3ee1d5710dcfc40d0b65f2f1f335c5fb
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-06-29 12:49:23 -0700

    add getUnreadMessages to dependency

--- src/app/(main)/(dashboard)/layout.tsx
+++ src/app/(main)/(dashboard)/layout.tsx
@@ -1,15 +1,37 @@
+'use client';
+
 import MainNavbar from '@/components/navs/MainNavbar';
 // import '../globals.css';
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 import MainFooter from '@/components/footers/MainFooter';
-export const metadata: Metadata = {
-    title: 'You & Me',
-    description: 'A chat application for ...',
-};
+import { useEffect } from 'react';
+import { useUserStore } from '@/store/userStore';
+// import { MessageType } from '@/types';
+import { useMessageStore } from '@/store/messageStore';
+// export const metadata: Metadata = {
+//     title: 'You & Me',
+//     description: 'A chat application for ...',
+// };
 
 const DefaultLayout = ({
     children,
 }: Readonly<{ children: React.ReactNode }>) => {
+    const { socket } = useUserStore();
+    const { getUnreadMessages } = useMessageStore();
+    useEffect(() => {
+        // handlers
+        const handleNewMessage = () => {
+            getUnreadMessages();
+        };
+
+        // events
+        socket.on('new-message', handleNewMessage);
+
+        // clean up the useEffect
+        return () => {
+            socket.off('new-message', handleNewMessage);
+        };
+    }, [socket, getUnreadMessages]);
     return (
         // <div className='w-screen h-screen bg-[#1C1B1B] text-[#FFFFFF] grid grid-rows-[1fr_8fr_1fr] overflow-hidden'>
         <div className='w-screen h-[100dvh] bg-[#1C1B1B] text-[#FFFFFF] grid grid-rows-[1fr_8fr_1fr] overflow-hidden'>
