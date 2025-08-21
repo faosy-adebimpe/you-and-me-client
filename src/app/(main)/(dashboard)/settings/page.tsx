15e4457c7344dbda077d9df824bbe400ce998af9
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-08-21 06:29:26 -0700

    remove full name field ...

--- src/app/(main)/(dashboard)/settings/page.tsx
+++ src/app/(main)/(dashboard)/settings/page.tsx
@@ -4,7 +4,7 @@ import InstallButton from '@/components/buttons/InstallButton';
 import MarkerIcon from '@/components/icons/MarkerIcon';
 import ChatLoader from '@/components/loaders/ChatLoader';
 import SilentLoader from '@/components/loaders/SilentLoader';
-import { joinNames } from '@/lib/utils';
+// import { joinNames } from '@/lib/utils';
 import { useUserStore } from '@/store/userStore';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
@@ -70,16 +70,16 @@ const SettingsPage = () => {
         return <ChatLoader />;
     }
     return (
-        <div className='px-3 w-full h-full overflow-y-scroll relative'>
+        <div className="px-3 w-full h-full overflow-y-scroll relative">
             <InstallButton />
 
-            <h1 className='text-2xl font-bold text-center p-4'>
+            <h1 className="text-2xl font-bold text-center p-4">
                 Account Settings
             </h1>
-            <div className='flex flex-col items-center mt-8'>
-                <div className='relative'>
+            <div className="flex flex-col items-center mt-8">
+                <div className="relative">
                     {uploadingProfilePicture && (
-                        <div className='absolute bg-black/50 w-full h-full rounded-full flex justify-center items-center'>
+                        <div className="absolute bg-black/50 w-full h-full rounded-full flex justify-center items-center">
                             <SilentLoader />
                         </div>
                     )}
@@ -94,91 +94,91 @@ const SettingsPage = () => {
                                     authUser.image) ||
                                 '/default-avatar.png'
                             }
-                            alt='Profile picture'
+                            alt="Profile picture"
                             width={96}
                             height={96}
-                            className='w-24 h-24 rounded-full object-cover border-4 border-gray-200'
+                            className="w-24 h-24 rounded-full object-cover border-4 border-gray-200"
                             unoptimized // Add this if you want to allow any remote image without domain config
                         />
                     ) : (
                         <Image
                             src={authUser?.image || '/default-avatar.png'}
-                            alt='Profile picture'
+                            alt="Profile picture"
                             width={96}
                             height={96}
-                            className='w-24 h-24 rounded-full object-cover border-4 border-gray-200'
+                            className="w-24 h-24 rounded-full object-cover border-4 border-gray-200"
                             unoptimized // Add this if you want to allow any remote image without domain config
                         />
                     )}
                     {!uploadingProfilePicture && (
                         <label
-                            htmlFor='avatar-upload'
-                            className='absolute bottom-0 right-0 bg-blue-600 text-white rounded-full p-2 cursor-pointer hover:bg-blue-700 transition flex justify-center items-center'
+                            htmlFor="avatar-upload"
+                            className="absolute bottom-0 right-0 bg-blue-600 text-white rounded-full p-2 cursor-pointer hover:bg-blue-700 transition flex justify-center items-center"
                         >
-                            <span className='text-[12px]'>
+                            <span className="text-[12px]">
                                 <MarkerIcon />
                             </span>
                             <input
-                                id='avatar-upload'
-                                type='file'
-                                accept='image/*'
-                                className='hidden'
+                                id="avatar-upload"
+                                type="file"
+                                accept="image/*"
+                                className="hidden"
                                 onChange={changeProfilePicture}
                             />
                         </label>
                     )}
                 </div>
-                <p className='mt-2 text-sm text-gray-500'>
+                <p className="mt-2 text-sm text-gray-500">
                     {uploadingProfilePicture
                         ? 'Changing profile picture...'
                         : 'Change profile picture'}
                 </p>
             </div>
 
-            <div className=''>
-                <div className='mt-8 w-full max-w-md mx-auto space-y-6 '>
-                    <div>
-                        <label className='block text-sm font-medium text-gray-700 mb-1'>
+            <div className="">
+                <div className="mt-8 w-full max-w-md mx-auto space-y-6 ">
+                    {/* <div>
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
                             Full Name
                         </label>
                         <input
-                            type='text'
+                            type="text"
                             value={joinNames(
                                 authUser.firstName,
                                 authUser.lastName
                             )}
                             disabled
-                            className='w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-700 cursor-not-allowed'
+                            className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-700 cursor-not-allowed"
                             readOnly
                         />
-                    </div>
+                    </div> */}
                     <div>
-                        <label className='block text-sm font-medium text-gray-700 mb-1'>
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
                             Username
                         </label>
                         <input
-                            type='text'
+                            type="text"
                             value={authUser?.username}
                             disabled
-                            className='w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-700 cursor-not-allowed'
+                            className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-700 cursor-not-allowed"
                             readOnly
                         />
                     </div>
                     <div>
-                        <label className='block text-sm font-medium text-gray-700 mb-1'>
+                        <label className="block text-sm font-medium text-gray-700 mb-1">
                             Email
                         </label>
                         <input
-                            type='email'
+                            type="email"
                             value={authUser?.email}
                             disabled
-                            className='w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-700 cursor-not-allowed'
+                            className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-100 text-gray-700 cursor-not-allowed"
                             readOnly
                         />
                     </div>
                     <button
-                        type='button'
-                        className='w-full mt-4 py-2 px-4 bg-blue-600 text-white rounded-md font-semibold hover:bg-blue-700 transition'
+                        type="button"
+                        className="w-full mt-4 py-2 px-4 bg-blue-600 text-white rounded-md font-semibold hover:bg-blue-700 transition"
                         disabled={uploadingProfilePicture || !interactedWith}
                         onClick={uploadProfilePicture}
                     >
@@ -186,11 +186,11 @@ const SettingsPage = () => {
                             ? 'Changing picture...'
                             : 'Update profile'}
                     </button>
-                    <div className='flex gap-6'>
+                    <div className="flex gap-6">
                         {!authUser?.verified && (
                             <button
-                                type='button'
-                                className='w-full mt-4 py-2 px-4 bg-green-600 text-white rounded-md font-semibold hover:bg-green-700 transition'
+                                type="button"
+                                className="w-full mt-4 py-2 px-4 bg-green-600 text-white rounded-md font-semibold hover:bg-green-700 transition"
                                 disabled={requestingVerificationEmail}
                                 onClick={handleEmailRequest}
                             >
@@ -200,8 +200,8 @@ const SettingsPage = () => {
                             </button>
                         )}
                         <button
-                            type='button'
-                            className='w-full mt-4 py-2 px-4 bg-red-600 text-white rounded-md font-semibold hover:bg-red-700 transition'
+                            type="button"
+                            className="w-full mt-4 py-2 px-4 bg-red-600 text-white rounded-md font-semibold hover:bg-red-700 transition"
                             disabled={loggingOut}
                             onClick={handleLogout}
                         >
